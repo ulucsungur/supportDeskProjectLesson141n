431f8f1eca0e74ae7cdf716546121a2ee4a8a20b
ulucsungur <144060671+ulucsungur@users.noreply.github.com>
2024-02-01 15:54:20 +0300

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const colors = require('colors');
+// package.json içindeki dotenv çağırılır
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000;
+
+// Connect to database
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Support Desk API' });
+});
+
+//Routes: userRoute.js teki router kullanmak için;
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
